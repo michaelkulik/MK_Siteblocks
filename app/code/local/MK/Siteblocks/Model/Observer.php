c3c62d861da38f8a170385d13704da99d9a78989
Michael <mishkulik@gmail.com>
2017-05-13 22:11:01 +0700

    Observer

---
+++ app/code/local/MK/Siteblocks/Model/Observer.php
@@ -0,0 +1,12 @@
+<?php
+
+class MK_Siteblocks_Model_Observer
+{
+    /**
+     * @param $observer Varien_Event_Observer
+     */
+    public function checkout_cart_product_add_after($observer)
+    {
+        var_dump($observer->getEvent()->getData('quote_item')->getData());die;
+    }
+}
